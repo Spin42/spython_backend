e415703488f3196ee0baa8e2727e972e9f6e1771
Xavier Defrang <xavier.defrang@gmail.com>
2013-11-13 22:50:54 +0100

    Changed property model

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,7 +1,12 @@
 class Property < ActiveRecord::Base
+
+  TYPES = %w(Text Number Image Location)
+
   # Associations
   belongs_to :enrichable, :polymorphic => true
 
   # Validations
-  validates_presence_of :key, :value
+  validates_presence_of :key, :type, :value
+  validates_inclusion_of :type, in: TYPES
+
 end
--- db/migrate/20131112220338_create_properties.rb
+++ db/migrate/20131112220338_create_properties.rb
@@ -2,8 +2,8 @@ class CreateProperties < ActiveRecord::Migration
   def change
     create_table :properties do |t|
       t.string :key,              :null => false
-      t.binary :value,            :null => false
-
+      t.string :type,             :null => false
+      t.text   :value,            :null => false, limit: 8.megabytes
       t.integer :enrichable_id,   :null => false
       t.string  :enrichable_type, :null => false
 
@@ -11,6 +11,7 @@ class CreateProperties < ActiveRecord::Migration
     end
 
     add_index :properties, :key
+    add_index :properties, :type
     add_index :properties, :enrichable_id
     add_index :properties, :enrichable_type
   end
