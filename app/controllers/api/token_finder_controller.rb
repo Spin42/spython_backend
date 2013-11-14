9ec2494cf64416b3a598eb17b1e1e8d7a9dceb23
Thibault Poncelet <thibaultponcelet@gmail.com>
2013-11-14 20:38:56 +0100

    Comment

--- app/controllers/api/token_finder_controller.rb
+++ app/controllers/api/token_finder_controller.rb
@@ -2,12 +2,12 @@ class Api::TokenFinderController < ApplicationController
   layout false
   respond_to :json
 
-  def index
-    respond_with @skin = Skin.where(token: params[:token])
-  end
+  # def index
+  #   respond_with @skin = Skin.where(token: params[:token])
+  # end
 
-  protected
-    def skin_params
-      params.require(:skin).permit(:token)
-    end
+  # protected
+  #   def skin_params
+  #     params.require(:skin).permit(:token)
+  #   end
 end
